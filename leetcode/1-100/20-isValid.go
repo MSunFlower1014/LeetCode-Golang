d5da94a7d257169031cc3cbdb9f60e2dd19d7d60
mayt <mayantao1996@126.com>
2020-06-23 17:48:04 +0800

    2020-6-23:彼之谓不道之道，此之谓不言之辩，故德总乎道之所一

--- leetcode/1-100/20-isValid.go
+++ leetcode/1-100/20-isValid.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 20. 有效的括号
 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
@@ -11,8 +13,28 @@ package main
 注意空字符串可被认为是有效字符串。
 */
 func main() {
-
+	print("")
+	fmt.Print(isValid("()"))
 }
 func isValid(s string) bool {
+	m := map[uint8]uint8{'(': ')', '[': ']', '{': '}'}
+	var queue []uint8
 
+	for i := range s {
+		length := len(queue)
+		if i == 0 || length == 0 {
+			queue = append(queue, s[i])
+			continue
+		}
+		if s[i] == m[queue[length-1]] {
+			queue = queue[0 : length-1]
+		} else {
+			queue = append(queue, s[i])
+		}
+	}
+	if len(queue) > 0 {
+		return false
+	} else {
+		return true
+	}
 }
