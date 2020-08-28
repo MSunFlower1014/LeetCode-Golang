ab3afb448c09ddfe317bed3797a37df1fbae5614
马艳涛 <yt.ma@goupwith.com>
2020-08-28 17:46:13 +0800

    2020-8-28:自定义排序

--- leetcode/1-100/56-merge.go
+++ leetcode/1-100/56-merge.go
@@ -1,10 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 /*
 56. 合并区间
 给出一个区间的集合，请合并所有重叠的区间。
-
-
 
 示例 1:
 
@@ -19,9 +22,52 @@ package main
 注意：输入类型已于2019年4月15日更改。 请重置默认代码定义以获取新方法签名。
 */
 func main() {
-
+	nums := [][]int{{1, 3}, {3, 6}}
+	fmt.Print(mergeIntervals(nums))
 }
 
 func mergeIntervals(intervals [][]int) [][]int {
+	//排序
+	mIntArray := &IntArray{intervals}
+	sort.Sort(mIntArray)
+	intervals = mIntArray.mArr
 
+	var result [][]int
+	var temp []int
+	for _, v := range intervals {
+		if len(temp) == 0 {
+			temp = v
+			continue
+		}
+		mid := v[0]
+		if mid >= temp[0] && mid <= temp[1] {
+			if v[1] > temp[1] {
+				temp[1] = v[1]
+			}
+			continue
+		}
+		result = append(result, temp)
+		temp = v
+	}
+	result = append(result, temp)
+	return result
+}
+
+type IntArray struct {
+	mArr [][]int
+}
+
+//IntArray实现sort.Interface接口
+func (arr *IntArray) Len() int {
+	return len(arr.mArr)
+}
+
+func (arr *IntArray) Swap(i, j int) {
+	arr.mArr[i], arr.mArr[j] = arr.mArr[j], arr.mArr[i]
+}
+
+func (arr *IntArray) Less(i, j int) bool {
+	arr1 := arr.mArr[i]
+	arr2 := arr.mArr[j]
+	return arr1[0] < arr2[0]
 }
